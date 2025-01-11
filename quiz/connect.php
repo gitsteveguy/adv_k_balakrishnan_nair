3c6a74a412fe3dce16c5d3ce240818da4655c7d4
Steve SajanJacob <121501747+gitsteveguy@users.noreply.github.com>
2025-01-11 11:48:05 +0530

    fallback for quiz

--- quiz/connect.php
+++ quiz/connect.php
@@ -1,5 +1,9 @@
 <?php
-$con = new mysqli('localhost', 'root', '', 'adv_k_balakrishnan_nair');
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
+try {
+    $con = new mysqli('localhost', 'root', '', 'adv_k_balakrishnan_nair');
+    if ($con->connect_error) {
+        // die("Connection failed: " . $con->connect_error);
+    }
+} catch (Exception $e) {
+    header("Location: https://advkbalakrishnannair.com/");
 }
