893defb56ba43ed481263a135366522def3f5276
Steve SajanJacob <121501747+gitsteveguy@users.noreply.github.com>
2025-01-17 01:04:26 +0530

    corrected to quizzes

--- quiz/participant_dashboard.php
+++ quiz/participant_dashboard.php
@@ -8,7 +8,7 @@ require_once("./participant_protect.php");
     <h2>Welcome <?php echo $_SESSION['user']['first_name'] ?></h2>
     <section class="dashboard-home-section grid">
         <div class="card btn" onclick="window.location.href='<?php echo $Globals['domain'] ?>/quiz/participant_view_quiz.php'">
-            <h2>Quizes</h2>
+            <h2>Quizzes</h2>
         </div>
         <div class="card btn" onclick="window.location.href='<?php echo $Globals['domain'] ?>/quiz/participant_view_results.php'">
             <h2>View Results</h2>
