e41e16e71483adad37acbbdf352ed044a85d7394
Steve SajanJacob <121501747+gitsteveguy@users.noreply.github.com>
2025-01-16 23:27:12 +0530

    changed whatsapp no

--- quiz/test.php
+++ quiz/test.php
@@ -1,16 +1,4 @@
 <?php
 require_once("./header.php");
-print_r($_SESSION['user']);
-
-?>
-<script>
-    function createWindow(src, width, height) {
-        var win = window.open(src, "_new", "width=" + width + ",height=" + height);
-        win.addEventListener("resize", function() {
-            console.log("Resized");
-            win.resizeTo(width, height);
-        });
-    }
-
-    createWindow("about:blank", 500, 300);
-</script>
+$hashed_password = password_hash('w421#@%&e2', PASSWORD_DEFAULT);
+echo $hashed_password;
